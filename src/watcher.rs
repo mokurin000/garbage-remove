7bff70e6a5b3dd74b732fb482457a4bba9303a35
mokurin000 <1348292515a@gmail.com>
2025-04-29 03:28:30 +0800

    log: log received paths

--- src/watcher.rs
+++ src/watcher.rs
@@ -3,6 +3,7 @@ use std::path::PathBuf;
 use ahash::AHashSet;
 use glob_match::glob_match;
 use kanal::Sender;
+use log::debug;
 use notify::EventHandler;
 
 pub struct Listener {
@@ -15,6 +16,7 @@ impl EventHandler for Listener {
     fn handle_event(&mut self, event: notify::Result<notify::Event>) {
         let Ok(event) = event else { return };
         for path in event.paths {
+            debug!("received {path:?} ");
             let path_str = path.to_string_lossy();
             if self.paths.contains(&path)
                 || self.globs.iter().any(|glob| glob_match(glob, &path_str))
